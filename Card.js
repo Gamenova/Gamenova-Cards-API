12ae281f4fc94a5740eddeaa4763f851752d1cb3
enrico <direction@4estudios.com>
2013-02-08 15:16:34 +0100

    added

---
+++ Card.js
@@ -0,0 +1,58 @@
+/**
+
+	DISCLAIMER HERE!!!!!
+*/
+
+/**
+	Card Class 
+	@constructor
+*/
+function Card(config){
+
+
+}
+
+/**
+	Deck Class 
+	@constructor
+*/
+function Deck(config){
+
+
+}
+
+/**
+	Hand Class 
+	@constructor
+*/
+function Hand(config){
+
+
+}
+
+/**
+	Pretable( HandChoices?) Class 
+	@constructor
+*/
+function Pretable(config){
+
+
+}
+
+/**
+	Table Class 
+	@constructor
+*/
+function Table(config){
+
+
+}
+
+/**
+	Rules Class 
+	@constructor
+*/
+function Rules(config){
+
+
+}
